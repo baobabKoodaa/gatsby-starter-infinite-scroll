a136a4e6a0217ea6d5c58455009c6cf2a8c33ed3
Atte Juvonen <atte.juvonen@futurice.com>
2019-03-07 15:00:14 +0200

    Icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        //TODO: icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
