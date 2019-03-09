e8a58447f05eaabbcea256edcc8f20d585687047
Atte Juvonen <atte.juvonen@futurice.com>
2019-03-09 21:31:53 +0200

    Minor (CSS).

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
-        margin: `0 auto`,
+        margin: `0 auto 2px`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
