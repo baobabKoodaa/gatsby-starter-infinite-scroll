59f8ad5ad7b00fc059302a4dc3d40cb4d21b048d
Kyle Turner <kyle.turner@trackvia.com>
2020-03-28 11:03:51 -0700

    Head true for tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-160649799-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
